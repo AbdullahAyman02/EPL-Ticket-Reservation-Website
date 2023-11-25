acbc0414f975f8cc1aa0ba6b4ce0821a389ce368
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2023-11-25 20:45:07 +0200

    Added the teams ednpoint and controller and modified the other endpoints and their controllers

--- Backend/routes/stadium.js
+++ Backend/routes/stadium.js
@@ -1,10 +1,12 @@
 import { Route, Router } from "express";
 import {
     getStadiums,
+    addStadium,
 } from "../controllers/stadiumController.js";
 
 const stadiumRouter = Router();
 
 stadiumRouter.get("/getStadiums", getStadiums);
+stadiumRouter.post("/addStadium", addStadium);
 
 export default stadiumRouter;
--- Backend/server.js
+++ Backend/server.js
@@ -5,22 +5,30 @@ import cookies from 'cookie-parser'
 import userRouter from "./routes/user.js";
 import matchRouter from "./routes/match.js";
 import refereeRouter from "./routes/referee.js";
+import teamRouter from "./routes/team.js";
 import stadiumRouter from "./routes/stadium.js";
 import verifyJWT from './middleware/verifyJWT.js'
-
+import cors from 'cors'
 dotenv.config();
-
+var corsOptions = {
+    origin: 'http://localhost:${process.env.PORT}/verify/${token}',
+    optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+  
+  
 // Test DB
 db.authenticate()
-    .then(() => console.log('Database connected...'))
-    .catch(err => console.log('Error: ' + err))
-
+.then(() => console.log('Database connected...'))
+.catch(err => console.log('Error: ' + err))
+  
 const app = express();
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookies());
 app.use(userRouter);
 app.use(matchRouter);
+app.use(teamRouter);
 app.use(refereeRouter);
 app.use(stadiumRouter);
 app.use(verifyJWT);
