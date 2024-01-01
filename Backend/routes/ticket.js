2f05822b5883e5ac6f7bd0c50f321b6408b0f383
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-01-02 01:06:40 +0200

    missed a bracket :(

--- Backend/routes/ticket.js
+++ Backend/routes/ticket.js
@@ -10,7 +10,7 @@ import {
 const ticketRouter = Router();
 
 ticketRouter.get("/getTicketsByMatchID/:id", verifyJWT(['M', 'F']), getTicketsByMatchID);
-ticketRouter.get("/getTicketsByUsername/:username", verifyJWT['M', 'F'], getTicketsByUsername);
+ticketRouter.get("/getTicketsByUsername/:username", verifyJWT(['M', 'F']), getTicketsByUsername);
 ticketRouter.use(verifyJWT(['F']));
 ticketRouter.post("/addTicket", addTicket);
 ticketRouter.delete("/deleteTicket/:ticket_no", deleteTicket);
