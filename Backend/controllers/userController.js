d767f8f320eb073edcf48d4a7bdb2608d6428999
AbdullahAyman02 <14712020102049@stud.cu.edu.eg>
2024-01-01 19:06:07 +0200

    Fixed Link in verification email

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -408,7 +408,7 @@ const SendEmail = async (req, res, type = 0) => {
             our website and entered your email.
             Your account has been approved!
             Please follow the given link to verify your email
-            https://epl-back.onrender.com/verify/${encodedToken} 
+            https://epl-ticket-reservation-website.vercel.app/verify/${encodedToken} 
             Thanks`,
     };
   }
