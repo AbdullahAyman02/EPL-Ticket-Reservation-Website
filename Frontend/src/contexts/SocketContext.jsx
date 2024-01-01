bedb834eff85766244fd5bd4c555295e5ce95eeb
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-01-01 17:17:05 +0200

    updating socket.io url in backend

--- Frontend/src/contexts/SocketContext.jsx
+++ Frontend/src/contexts/SocketContext.jsx
@@ -6,7 +6,7 @@ export const SocketContext = createContext();
 // eslint-disable-next-line react/prop-types
 export const SocketContextProvider = ({ children }) => {
   let socket = useRef(null);
-  socket.current = io("https://epl-back.onrender.com", {
+  socket.current = io("wss://epl-back.onrender.com", {
     transports: ["websocket"],
   });
 
