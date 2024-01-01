09c6bac6811cffa9f050f0cd36f16f3921a350d2
MahmoudSamy1452 <mahmoud.abdelwahed02@eng-st.cu.edu.eg>
2024-01-02 00:14:56 +0200

    Added a loading screen

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
+import { useState } from "react";
+import axios from "axios";
 import Navbar from "./components/Navbar";
-
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import NotFound from "./pages/404";
@@ -18,39 +18,61 @@ import MyTickets from "./pages/MyTickets";
 import Admin from "./pages/Admin";
 import MatchDetails from "./pages/MatchDetails.jsx";
 import { SocketContextProvider } from "./contexts/SocketContext";
+import spinner from "./assets/spinner.png";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  axios.interceptors.request.use((config) => {
+    setIsLoading(true);
+    return config;
+  });
+
+  axios.interceptors.response.use(
+    (response) => {
+      setIsLoading(false);
+      return response;
+    },
+    (error) => {
+      setIsLoading(false);
+      return Promise.reject(error);
+    }
+  );
   return (
-    <AuthContextProvider>
-      <BrowserRouter>
-        <Navbar />
-        <div id="hero">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/signup" element={<SignUp />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/schedule" element={<Schedule />} />
-            <Route path="/matchDetails" element={<MatchDetails />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route
-              path="/reservation/:match_id"
-              element={
-                <SocketContextProvider>
-                  <Reservation />
-                </SocketContextProvider>
-              }
-            />
-            <Route path="/verify/:token" element={<EmailVerification />} />
-            <Route path="/match/create" element={<CreateMatch />} />
-            <Route path="/mytickets" element={<MyTickets />} />
-            <Route path="/match/update/:match_id" element={<EditMatch />} />
-            <Route path="/stadium/create" element={<CreateStadium />} />
-            <Route path="/admin" element={<Admin />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </div>
-      </BrowserRouter>
-    </AuthContextProvider>
+    <>
+      <div className={`loader ${isLoading ? "" : "fadeOut"}`}>
+        <img className="spinner inline" src={spinner}></img>
+      </div>
+      <AuthContextProvider>
+        <BrowserRouter>
+          <Navbar />
+          <div id="hero">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/signup" element={<SignUp />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/schedule" element={<Schedule />} />
+              <Route path="/matchDetails" element={<MatchDetails />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route
+                path="/reservation/:match_id"
+                element={
+                  <SocketContextProvider>
+                    <Reservation />
+                  </SocketContextProvider>
+                }
+              />
+              <Route path="/verify/:token" element={<EmailVerification />} />
+              <Route path="/match/create" element={<CreateMatch />} />
+              <Route path="/mytickets" element={<MyTickets />} />
+              <Route path="/match/update/:match_id" element={<EditMatch />} />
+              <Route path="/stadium/create" element={<CreateStadium />} />
+              <Route path="/admin" element={<Admin />} />
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </div>
+        </BrowserRouter>
+      </AuthContextProvider>
+    </>
   );
 }
 
